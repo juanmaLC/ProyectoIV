e1df2d14ed8601135bd5416914afcc551e3ec7e2
Juan Manuel Lopez Castro <lopezjuanma03@gmail.com>
2018-10-14 16:42:51 +0200

    closes #6

---
+++ lib/test1.rb
@@ -0,0 +1,45 @@
+class Clases
+
+	
+
+
+	def initialize(modalidad,profesor,disponible)
+
+		@modalidad=modalidad
+		@profesor=profesor
+		@disponible=disponible
+	
+	end
+
+
+	def mostrarProfesor
+		
+		puts "#{@profesor}"
+	end
+
+
+	def mostrarModalidad
+		
+		puts "#{@modalidad}"	
+	end
+
+	def mostrarDisponibilidad
+		
+		puts "#{@disponible}"
+	end
+
+
+	
+
+
+	
+	
+
+
+end
+
+
+
+
+
+
