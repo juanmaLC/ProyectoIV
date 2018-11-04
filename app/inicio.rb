f57b267c4a516cbfee185f179f6f75891cb6bf1e
Juan Manuel Lopez Castro <lopezjuanma03@gmail.com>
2018-11-04 17:15:08 +0100

    configurando

--- app/inicio.rb
+++ app/inicio.rb
@@ -1,12 +1,11 @@
 require 'rubygems'
-require 'sinatra/base'
 require 'sinatra'
 
 
-class Inicio < Sinatra::Base
+
 
 get '/' do
   "Hello World"
 end
 
-end
+
--- config.ru
+++ config.ru
@@ -1,6 +1,7 @@
 require 'rubygems' 
 require 'sinatra' 
 require 'bundler'
-Bundler.require
+
 require './app/inicio' 
 
+run Sinatra::Application
