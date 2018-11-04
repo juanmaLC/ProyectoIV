11bbf87443640e4b5fc927e2e5657385acdc7621
Juan Manuel Lopez Castro <lopezjuanma03@gmail.com>
2018-11-04 02:07:34 +0100

    restauro version

--- app/app.rb
+++
@@ -1,6 +0,0 @@
-require 'sinatra'
-
-get '/' do
-  "Microservicio de Infraestructura Virtual en desarrollo"
-end
-
--- config.ru
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'sinatra'
-require 'bundler'
-Bundler.require
-require './app/app'
-
