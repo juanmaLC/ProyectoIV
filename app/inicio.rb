43cf5c0722d4b6edff1ebfa36530f8b7eee35178
Juan Manuel Lopez Castro <lopezjuanma03@gmail.com>
2018-11-11 14:21:59 +0100

    closes #19

--- app/inicio.rb
+++ app/inicio.rb
@@ -7,3 +7,13 @@ get '/' do
 	{:status => 'ok'}.to_json
 end
 
+
+
+get '/informacion' do
+
+	content_type :json
+	{ :informacion => 'Microservicio web de la asignatura Infraestructura Virtual. En los proximos hitos se le añadira clases a este proyecto con diferentes funcionalidades , relaccionadas con la disponibilidad y horario de clases ofrecidas en un gimnasio',
+	  :version => 'hito 3 del proyecto' }.to_json
+
+end 
+
