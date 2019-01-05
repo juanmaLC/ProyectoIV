118f4697d67be45a74cbbf9e4a1968d39c5c3fcd
Juan Manuel Lopez Castro <lopezjuanma03@gmail.com>
2019-01-05 20:52:21 +0100

    closes #43

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,6 +1,6 @@
 require './lib/ClasesGym'  
-
 require 'json'
+
 
 
 
@@ -17,6 +17,7 @@ clase["espacios_libres"],clase["clase"],clase["duracion"])
 	contador=contador+1
 
 }
+
 
 
 
@@ -76,6 +77,91 @@ describe 'Inicializacion de clase correcta : ' do
 
 	end
 
+end
+
+
+
+
+describe 'Rutas GET | POST | PUT | DELETE correctas : ' do
+
+	
+
+	it 'GET /' do
+		
+		expect(:status => "ok").not_to be_empty
+	end
+
+	it 'GET /status' do
+		
+		expect(:status => "ok").not_to be_empty
+	end
+
+	it 'GET /informacion' do
+		
+		expect(:informacion => "Microservicio web de la asignatura Infraestructura Virtual con diferentes funcionalidades , relaccionadas con la disponibilidad y horario de clases ofrecidas en un gimnasio y modificacion de dichas clases",
+			:version => "hito 5 del proyecto").not_to be_empty
+
+	end
+
+
+	it 'GET /clases ' do
+
+		expect({"_id":{"$oid":"5c2f6b975c89472964867c29"},"modalidad":"natacion","profesor":"pepe","disponible":"si","espacios":"10","clase":"1.1","duracion":"10min"}).not_to be_empty
+	end
+
+
+
+	it 'GET /clase/:id/?' do
+		
+		expect({"_id":{"$oid":"5c2f6b975c89472964867c29"},"modalidad":"natacion","profesor":"pepe","disponible":"si","espacios":"10","clase":"1.1","duracion":"10min"}).not_to be_empty
+		
+	end
+
+
+	it 'GET /InformacionClase1' do
+
+		expect(clases[0].to_json).not_to be_empty
+
+	end
+
+
+	it 'GET /InformacionClase2' do
+
+		expect(clases[1].to_json).not_to be_empty
+
+	end
+
+	it 'GET /InformacionClase3' do
+
+		expect(clases[2].to_json).not_to be_empty
+
+	end
+
+
+	it 'POST /crearActividad?modalidad=cross&profesor =natalia&disponible=si&espacios=4&clase=1.0&duracion =1h' do 
+		
+		
+		expect({"_id":{"$oid":"5c3108765c89471828d9cece"},"modalidad":"cross","profesor ":"natalia","disponible":"si","espacios":"4","clase":"1.0","duracion ":"1h"}).not_to be_empty
+	
+	end
+
+
+	it 'DELETE /eliminarActividad/:id/?' do
+		expect(:success => "Eliminado con exito /clases para comprobar que ha sido borrada").not_to be_empty
+		expect(:success => "No se pudo borrar , id invalida o inexistente").not_to be_empty
+		
+	end
+
+
+	it 'PUT /actualizarActividad/?' do
+		
+		expect({"_id":{"$oid":"5c3108765c89471828d9cece"},"modalidad":"running","profesor ":"natalia","disponible":"si","espacios":"4","clase":"1.0","duracion ":"1h"}).not_to be_empty
+		
+		
+	end
+	
 
 
 end
+
+
