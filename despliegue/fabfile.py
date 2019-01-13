9b15069889d6e62bd766fa43151688e80e4c16db
Juan Manuel Lopez Castro <lopezjuanma03@gmail.com>
2019-01-13 12:37:11 +0100

    closes #51

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -1,39 +1,36 @@
-from __future__ import with_statement
-from fabric.api import *
-from fabric.contrib.console import confirm
-
-#Lugar y usuario con el que nos vamos a conectar
-env.user='vagrant'
-env.hosts = ['proyectoivgym.westeurope.cloudapp.azure.com']
-
-
-
+from fabric.api import run
 
 
 
 def BorrarCodigo():
-	run("sudo rm -rf ProyectoIV")
+	run("rm -rf ProyectoIV")
 
 
 def ClonarCodigo():
-	run("sudo git clone https://github.com/juanmaLC/ProyectoIV.git")
+	run("git clone https://github.com/juanmaLC/ProyectoIV.git")
 
-def InstalarMongoDB():
-	run("sudo apt-get install -y mongodb")
+def IniciarMongoDB():
 	run("sudo systemctl start mongodb")
 
+def DetenerMongoDB():
+	run("sudo systemctl stop mongodb")
+
 def InstalarMicroservicio():
-	BorrarCodigo()
-	ClonarCodigo()
-	InstalarMongoDB()
-	run("sudo apt install gcc")
-	run("sudo apt install make")
-	run("sudo apt install ruby-dev")
-	run("sudo gem install bundler")
 	run("cd ProyectoIV/ && bundle install")
 
 def IniciarMicroservicio():
+
+	#Para que siempre que iniciemos el servicio la base de datos este operativa
+	IniciarMongoDB()
 	run ("cd ProyectoIV/ && sudo bundle exec rackup -p 80 --host 0.0.0.0")
+
+def DetenerMicroservicio():
+	#run("")
+
+def ReiniciarMicroservicio():
+	DetenerMicroservicio()
+	IniciarMicroservcio()
+	
 	
 
 	
